80aac71ea749d0132de26429a16be0ec35a2c51b
Nataly Silvana <n.silvana11@gmail.com>
2025-06-23 17:29:33 +0200

    ahora si

--- Nombre_&_Mascotas.py
+++ Nombre_&_Mascotas.py
@@ -1,21 +1,20 @@
-#Vamos a ver quien eres y tu mascota
-nombre = input("Como te llamas: ")
+nombre = input("¿Cómo te llamas? ")
+nombres_validos = ["nataly", "natalie", "andrea"]
 
-if nombre == "Nataly":
-    print("Hola Nataly")
+if nombre.lower() in nombres_validos:
+    print(f"Hola {nombre}, ¡qué bueno conocerte!")
+
     animal = input("¿Tu mascota es un gato o un perro? ")
-    if animal == "gato" or animal == "Gato":
-        print("Tu mascota es un gato")
-        print("Claro es Simba!")
+    if animal.lower() == "gato":
+        print("Tu mascota es un gato.")
+        print("¡Claro, es Simba!")
     else:
-        print("No lo conozco, pero seguro es genial!")
+        print("No tienes perro.")
+else:
+    print("Hola, no te conozco.")
+    tiene_mascota = input("¿Tienes mascota? (sí/no) ")
 
-
-
-elif nombre is not ["Nataly", "Natalie"]:
-    print("Hola, no te conozco")
-    animal = input("¿Tienes una mascota? (si/no) ")
-    if animal.lower() == "si":
-        print("Genial, siemprees es bueno tener una mascota.")
-    else:
+    if tiene_mascota.lower() == "sí":
+        print("Genial, siempre es bueno tener una mascota.")
+    elif tiene_mascota.lower() == "no":
         print("Está bien, no todos tienen mascotas.")
