adc195e145e6bb7edd4f218ea1b11a5c5890c6d6
Nataly Silvana <n.silvana11@gmail.com>
2025-06-23 16:34:22 +0200

    Esto es para realizar calculos

---
+++ calculos.py
@@ -0,0 +1,20 @@
+n1 = 10
+n2 = 32
+
+suma_numeros = n1 + n2
+print ("La suma es: ", suma_numeros)
+
+resta_numeros = n1 - n2
+print ("La resta es: ", resta_numeros)
+
+producto_numeros = n1 * n2
+print ("El producto es: ", producto_numeros)
+
+division_numeros = n1 / n2
+print ("La división es: ", division_numeros)
+
+division_entera_numeros = n1 // n2
+print ("La división entera es: ", division_entera_numeros)
+
+modulo_numeros = n1 % n2
+print ("El módulo es: ", modulo_numeros)
