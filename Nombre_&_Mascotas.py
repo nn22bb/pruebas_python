c7b2602245f9d82d05c269b404483f70ca9a2c15
nn22bb <120388974+nn22bb@users.noreply.github.com>
2025-06-23 17:02:08 +0200

    Update Nombre_&_Mascotas.py
    Un pequeño arreglo

--- Nombre_&_Mascotas.py
+++ Nombre_&_Mascotas.py
@@ -4,18 +4,16 @@ nombre = input("Como te llamas: ")
 if nombre == "Nataly":
     print("Hola Nataly")
     animal = input("¿Tu mascota es un gato o un perro? ")
-    if animal == "gato":
+    if animal == "gato" or animal == "Gato":
         print("Tu mascota es un gato")
         print("Claro es Simba!")
     else:
         print("No lo conozco, pero seguro es genial!")
 
-
-
-elif nombre not in ["Nataly", "Natalie"]:
+elif nombre is not ["Nataly", "Natalie"]:
     print("Hola, no te conozco")
     animal = input("¿Tienes una mascota? (si/no) ")
     if animal.lower() == "si":
-        print("Genial, ¿qué tipo de mascota tienes?")
+        print("Genial, siemprees es bueno tener una mascota.")
     else:
         print("Está bien, no todos tienen mascotas.")
